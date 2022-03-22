5fe5078b23b9e3332717b7667d65c3fa33d18597
bcpham <bc.pham@hotmail.com>
2022-03-21 21:52:29 -0700

    Updated melons.py

---
+++ melon_info.py
@@ -0,0 +1,32 @@
+"""Print out all the melons in our inventory."""
+
+
+#from melons import melon_names, melon_prices, melon_seedlessness 
+
+
+# def print_melon(name, price, seedless):
+#     """Print each melon with corresponding attribute information."""
+
+#     have_or_have_not = 'have'
+#     if seedless:
+#         have_or_have_not = 'do not have'
+
+#     print(f'{name}s {have_or_have_not} seeds and are ${price}')
+
+
+# for i in melon_names:
+#     print_melon(melon_names[i], melon_prices[i], melon_seedlessness[i])
+
+from melons import melon_informat
+
+def print_melon(melon_name):
+
+    for melon_names in melon_format:
+        if melon_name == melon_format[0]:
+            print(melon_format[0] \n + "price: " + 
+            "seedless: " +
+            "flesh_color: " +
+            "rind_color: " +
+            "average_weight: " + )
+
+print_melon("Honeydew")
---
+++ melons.py
@@ -0,0 +1,33 @@
+# melon_names = {
+#     1: 'Honeydew',
+#     2: 'Crenshaw',
+#     3: 'Crane',
+#     4: 'Casaba',
+#     5: 'Cantaloupe',
+# }
+
+# melon_prices = {
+#     1: 0.99,
+#     2: 2.00,
+#     3: 2.50,
+#     4: 2.50,
+#     5: 0.99,
+# }
+
+# melon_seedlessness = {
+#     1: True,
+#     2: False,
+#     3: False,
+#     4: False,
+#     5: False,
+# }
+
+##########################################################################
+
+melon_informat = {
+    "Honeydew":(0.99, True, None, None, None), 
+    "Crenshaw":(2.00, False, None, None, None),
+    "Crane":(2.50, False, None, None, None),
+    "Casaba":(2.50, False, None, None, None),
+    "Cantaloupe":(0.99, False, None, None, None)
+}
